5a843b5986bc5ce524c312b33359e0896293591c
Nikita Rogalenko <nikitalr@mail.ru>
2022-11-22 00:34:20 +0300

    evaluating search - count metrics

--- task1/main.py
+++ task1/main.py
@@ -1,12 +1,15 @@
 import warnings
 from doc_collection_processor import process_doc_collection
 from elastic import connect_elasticsearch, create_indexes_if_missing
+from search_evaluator import evaluate_search
 
 
 DOCUMENTS_COLLECTION_PATH = "../resources/byweb_for_course"
+SEARCH_EVALUATION_RESULTS_PATH = "../resources/evaluation_metrics.txt"
 
 if __name__ == "__main__":
     warnings.filterwarnings('ignore', message="Unverified HTTPS request is being made to host*")
     es_obj = connect_elasticsearch()
     create_indexes_if_missing(es_obj)
     process_doc_collection(DOCUMENTS_COLLECTION_PATH, es_obj, need_raw=True, need_stemmed=True)
+    evaluate_search(es_obj, SEARCH_EVALUATION_RESULTS_PATH)
---
+++ task1/search_evaluator.py
@@ -0,0 +1,100 @@
+import xml.etree.ElementTree as ET
+
+
+TASKS_PATH = '../resources/web2008_adhoc.xml'
+RELEVANT_DOCS_PATH = '../resources/relevant_table_2009.xml'
+K = 20
+
+'''
+"query": {
+            "query_string": {
+                "query": text,
+                "fields": ["content"]
+            }
+        }
+'''
+
+
+def get_search_query(text, size):
+    return {
+        "from": 0, "size": size,
+        "query": {
+            "match": {
+                "content": {
+                    "query": text
+                }
+            }
+        }
+    }
+
+
+def count_metrics_for_task(es_obj, index_name, query_text, retrieved_documents, relevant_documents_lookup):
+    true_relevant = 0
+    total_relevant_docs_count = len([doc_info for doc_info in relevant_documents_lookup
+                                     if relevant_documents_lookup[doc_info] == 'vital'])
+
+    for doc_id in retrieved_documents:
+        if doc_id in relevant_documents_lookup and relevant_documents_lookup[doc_id] == 'vital':
+            true_relevant += 1
+    p_k = true_relevant/K
+    # print("p@20: " + str(p_k))
+    r_k = round(true_relevant/total_relevant_docs_count, 2) if total_relevant_docs_count != 0 else 0.0
+    # print("r@20: " + str(r_k))
+
+    r_precision_search_result = es_obj.search(index=index_name, body=get_search_query(query_text, total_relevant_docs_count))
+    r_precision_retrieved_documents_ids = [result['_source']['doc_id'] for result in r_precision_search_result['hits']['hits']]
+    r_precision_relevant = 0
+    for doc_id in r_precision_retrieved_documents_ids:
+        if doc_id in relevant_documents_lookup and relevant_documents_lookup[doc_id] == 'vital':
+            r_precision_relevant += 1
+    r_precision = round(r_precision_relevant/total_relevant_docs_count, 2) if total_relevant_docs_count != 0 else 0.0
+    # print("R-precision: " + str(r_precision))
+    return [p_k, r_k, r_precision]
+
+
+def evaluate_search(es_obj, results_path):
+    raw_results_dict = {}
+    stemmed_results_dict = {}
+    search_tasks, relevant_docs_for_tasks = parse_xml_files()
+    print("Start evaluation")
+    for task in search_tasks:
+        print(search_tasks[task])
+        raw_search_task_result = es_obj.search(index="raw", body=get_search_query(search_tasks[task], K))
+        stemmed_search_task_result = es_obj.search(index="stemmed", body=get_search_query(search_tasks[task], K))
+        raw_retrieved_documents_ids = [result['_source']['doc_id'] for result in raw_search_task_result['hits']['hits']]
+        stemmed_retrieved_documents_ids = [result['_source']['doc_id']
+                                           for result in stemmed_search_task_result['hits']['hits']]
+        raw_results_dict[search_tasks[task]] = \
+            count_metrics_for_task(es_obj, "raw", search_tasks[task], raw_retrieved_documents_ids, relevant_docs_for_tasks[task])
+        stemmed_results_dict[search_tasks[task]] = \
+            count_metrics_for_task(es_obj, "stemmed", search_tasks[task], stemmed_retrieved_documents_ids, relevant_docs_for_tasks[task])
+    map_k_raw = round(sum([raw_results_dict[task][0] for task in raw_results_dict]) / len(raw_results_dict), 3)
+    map_k_stemmed = round(sum([stemmed_results_dict[task][0] for task in stemmed_results_dict]) / len(stemmed_results_dict), 3)
+    results_file = open(results_path, "w")
+    results_file.write('Raw index MAP@20: {}. Stemmed index MAP@20: {}\n'.format(map_k_raw, map_k_stemmed))
+    results_file.write('Task    p@20(raw/stemmed)   r@20(raw/stemmed)   r-precision(raw/stemmed)\n')
+    for task in raw_results_dict:
+        results_file.write('{}  {}/{}   {}/{}   {}/{}\n'.format(task, raw_results_dict[task][0],
+                                                                stemmed_results_dict[task][0], raw_results_dict[task][1],
+                                                                stemmed_results_dict[task][1], raw_results_dict[task][2],
+                                                                stemmed_results_dict[task][2]))
+    results_file.close()
+
+
+def parse_xml_files():
+    tasks_tree = ET.parse(TASKS_PATH)
+    tasks_root = tasks_tree.getroot()
+    relevant_docs_tree = ET.parse(RELEVANT_DOCS_PATH)
+    relevant_docs_root = relevant_docs_tree.getroot()
+    search_tasks = {}
+    for child in tasks_root[1:]:
+        search_tasks[child.attrib.get('id')] = child[0].text
+    relevant_docs_for_tasks = {}
+    for task in relevant_docs_root:
+        if task.attrib.get('id') in search_tasks:
+            docs_dict = {}
+            for document in task:
+                docs_dict[document.attrib.get('id')] = document.attrib.get('relevance')
+            relevant_docs_for_tasks[task.attrib.get('id')] = docs_dict
+    search_tasks_only_with_documents_present = {key: search_tasks[key] for key in relevant_docs_for_tasks.keys()}
+    return search_tasks_only_with_documents_present, relevant_docs_for_tasks
